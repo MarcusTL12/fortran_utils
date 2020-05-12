68b532fd2b57b7e3c04aae142bab08817e6aab2d
MarcusTL12 <marcuspaafjellet@gmail.com>
2020-05-12 18:45:39 +0200

    better sum of divisors

--- src/math_util.f90
+++ src/math_util.f90
@@ -5,8 +5,9 @@ module math_util_mod
     private
     !
     public :: next_permutation, clamp, prime, factorize, next_factor, &
-              sum_divisors
+              sum_divisors, nextprime, isprime
     !
+    integer, parameter :: amt_primes = 1230
     integer, allocatable :: primes(:)
     !
     public :: inc
@@ -79,7 +80,6 @@ contains
     subroutine load_primes()
         implicit none
         !
-        integer, parameter :: amt_primes = 1230
         integer :: p, i
         !
         allocate (primes(amt_primes))
@@ -103,16 +103,74 @@ contains
         prime = primes(i)
     end function
     !
+    logical function isprime(n)
+        implicit none
+        !
+        integer, intent(in) :: n
+        integer :: i
+        !
+        isprime = .true.
+        do i = 1, amt_primes
+            if (n == prime(i)) return
+            if (modulo(n, prime(i)) == 0) then
+                isprime = .false.
+                return
+            end if
+        end do
+        !
+        do i = prime(amt_primes) + 2, ceiling(sqrt(real(n))), 2
+            if (modulo(n, i) == 0) then
+                isprime = .false.
+                return
+            end if
+        end do
+    end function
+    !
+    integer function nextprime(n) result(m)
+        implicit none
+        !
+        integer, intent(in) :: n
+        !
+        m = n
+        if (isprime(m)) return
+        if (modulo(m, 2) == 0) m = m + 1
+        !
+        do while (.not. isprime(m))
+            m = m + 2
+        end do
+    end function
+    !
     subroutine next_factor(n, i, p, e)
         implicit none
         !
         integer, intent(inout) :: n
-        integer, intent(out) :: p, e
+        integer, intent(inout) :: p, e
         integer, intent(inout) :: i
+        integer :: pr
         !
-        do while (prime(i) <= n)
-            if (modulo(n, prime(i)) == 0) then
-                p = prime(i)
+        if (i <= amt_primes) then
+            do while (prime(i) <= n)
+                if (modulo(n, prime(i)) == 0) then
+                    p = prime(i)
+                    e = 1
+                    n = n / p
+                    !
+                    do while (modulo(n, p) == 0)
+                        e = e + 1
+                        n = n / p
+                    end do
+                    return
+                end if
+                !
+                i = i + 1
+                if (i > amt_primes) exit
+            end do
+        end if
+        !
+        pr = nextprime(p + 1)
+        do while (pr <= n)
+            if (modulo(n, pr) == 0) then
+                p = pr
                 e = 1
                 n = n / p
                 !
@@ -120,10 +178,10 @@ contains
                     e = e + 1
                     n = n / p
                 end do
-                exit
+                return
             end if
             !
-            i = i + 1
+            pr = nextprime(pr + 2)
         end do
     end subroutine
     !
@@ -151,14 +209,33 @@ contains
         implicit none
         !
         integer, intent(in) :: n
-        integer :: m, i, p, e
+        integer :: m, i, j, p, e, s, r
         !
         m = n
         i = 1
+        p = 1
         sum_divisors = 1
         do while (m > 1)
             call next_factor(m, i, p, e)
-            sum_divisors = sum_divisors * (p**(e + 1) - 1) / (p - 1)
+            s = 0
+            r = 1
+            do j = 0, e
+                s = s + r
+                r = r * p
+            end do
+            sum_divisors = sum_divisors * s
         end do
+    contains
+        integer function ipow(b, e)
+            implicit none
+            !
+            integer, intent(in) :: b, e
+            integer :: i
+            !
+            ipow = 1
+            do i = 1, e
+                ipow = ipow * b
+            end do
+        end function
     end function
 end module
--- tests.f90
+++ tests.f90
@@ -514,6 +514,6 @@ contains
         use iso_c_binding
         implicit none
         !
-        print *, sum_divisors(5040)
+        print *, sum_divisors(100004)
     end subroutine
 end program main
